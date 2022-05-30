1d4c5cba702912929851ed0094ecb56957aa672b
arshptl <arshpatel714@gmail.com>
2022-05-30 20:31:50 +0530

    types for variables

---
+++ features/variables.ts
@@ -0,0 +1,32 @@
+let apple: number = 10;
+let color: string = 'red';
+let hasName: boolean = true;
+
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+
+// built in objects
+let now: Date = new Date();
+
+
+// Array (Type annotation for an array)
+let colors: string[] = ['red', 'green', 'blue'];
+let marks: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+
+// Classes
+
+class Car{
+
+}
+
+const car = new Car();
+
+// Object literal
+const point: {x: number, y: number} = {
+    x: 10,
+    y: 20
+}
